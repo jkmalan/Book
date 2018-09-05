a2f9a429309231ffddcc95f5c42dbf144fc71571
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2018-09-05 14:06:12 -0400

    Updated Design of Book class Fixed some bad spacing

--- src/com/jkmalan/Book/Client.java
+++ src/com/jkmalan/Book/Client.java
@@ -2,27 +2,27 @@ package com.jkmalan.Book;
 
 public class Client {
 
-	byte me;
-	String cheese;
-	int eger;
-	double jeopardy;
-	long john_silver;
-	
-	public static void main(String[] args)
-	{
-		
-		Book book1 = new Book("gr8 gatsbi", 10);
-		
-		for(int i = 0;i < 10; i++)
-		{
-			
-			book1.getCurrentPage().setContents("gr8 m8");
-			book1.flipForwards();
-			
-		}
-		
-		System.out.println(book1);
-		
-	}
+    byte me;
+    String cheese;
+    int eger;
+    double jeopardy;
+    long john_silver;
+
+    public static void main(String[] args)
+    {
+
+        Book book = new Book("gr8 gatsbi", 10);
+
+        for(int i = 0;i < 10; i++)
+        {
+
+            book.getCurrentPage().setContents("gr8 m8");
+            book.flipForwards();
+
+        }
+
+        System.out.println(book);
+
+    }
 	
 }
